b9e4f1cc995bbf38adeb0c50b36b3e14c5451a8e
rohit-pandey45 <rpandey1627@gmail.com>
2026-07-05 12:42:00 +0530

    DAY 1

--- Dynamic Programming/Day 1/01 Knapsack Problem.cpp
+++ Dynamic Programming/Day 1/01 Knapsack Problem.cpp
@@ -1,3 +1,42 @@
+//Tabulation Approach
+#include <bits/stdc++.h>
+using namespace std; 
+
+
+int branch_and_bound(int n, int wmax, vector<pair<int,int>> items) {
+    vector<vector<int>> dp(n, vector<int> (wmax+1,0));
+    
+    //BASE CASE
+    for(int w = items[0].first; w <= wmax; w++){
+        dp[0][w] = items[0].second;
+    }
+    
+    for(int ind = 1; ind < n; ind++){
+        for(int w = 0; w <= wmax; w++){
+            int notTake = dp[ind-1][w];
+            int take = INT_MIN;
+            if(items[ind].first <= w){
+                take = items[ind].second + dp[ind-1][w - items[ind].first];
+            }
+            dp[ind][w] = max(take,notTake);
+        }
+    }
+    return dp[n-1][wmax];
+}
+
+int main() {
+    int n; cin >> n;
+    int wmax; cin >> wmax;
+    vector<pair<int,int>> items(n);
+    for(int i = 0; i < n; i++) {
+        cin >> items[i].first >> items[i].second;
+    }
+    cout << branch_and_bound(n, wmax, items);
+
+    return 0;
+}
+
+
 /**
 Memoisation Approach
 
